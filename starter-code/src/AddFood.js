82243501219b5b45038d40a9681bea7f9345e833
Nicholas Crispino <crispinonicky@gmail.com>
2018-09-26 10:11:56 -0400

    got iterations 1 and 2 done, but can't get items to appear on page

---
+++ starter-code/src/AddFood.js
@@ -0,0 +1,34 @@
+// import React, { Component } from "react";
+// import "./App.css";
+
+
+// class AddFood extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {}
+//   }
+
+
+//   render(){
+//     <div className="add-food">
+
+//     <form >
+//     <div className="title">Add a New Food</div>
+
+//     <label>Name</label>
+//     <input onChange = {e=>this.handleNameChange(e)} value={this.state.foodNameInput}type="text" />
+
+//     <label>Calories</label>
+//     <input  onChange = {e=>this.handleCaloriesChange(e)} value={this.state.caloriesInput} type="number" />
+
+//     <label>Image</label>
+//     <input  onChange = {e=>this.handleImageChange(e)} value={this.state.imageInput} type="text" />
+
+//     <button>Submit</button>
+//   </form>
+//   </div>
+
+//   }
+// }
+
+// export default AddFood;
--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import foods from "./foods.json";
+import FoodBox from "./FoodBox";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foodList: foods,
+      foodNameInput: "",
+      caloriesInput: "",
+      imageInput: ""
+    };
+  }
+
+  showFoods = () => {
+    return this.state.foodList.map((eachFood, index) => {
+      return (
+        <FoodBox
+          key={index}
+          foodName={eachFood.name}
+          calories={eachFood.calories}
+          image={eachFood.image}
+        />
+      );
+    });
+  };
+
+
+  handleCaloriesChange = (e) =>{
+    this.setState({caloriesInput:e.target.value})
+  }
+
+  handleNameChange = (e) =>{
+    this.setState({foodNameInput:e.target.value})
+
+
+  }
+
+  handleImageChange = (e) =>{
+    this.setState({imageInput:e.target.value})
+
+
+  }
+
+  addNewFood= (e) => {
+    e.preventDefault();
+    const newFood = {
+      name: this.state.foodNameInput,
+      calories: this.state.caloriesInput,
+      image: this.state.imageInput
+    }
+    
+    console.log(newFood);
+
+      const copyOfFoodList = [...this.state.foodList];
+      
+      copyOfFoodList.unshift(newFood);
+    
+    
+      this.setState({foodList: copyOfFoodList});
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="title">
+          <h1>Welcome to IronSnack</h1>
+<div className="add-food">
+          <form >
+            <div className="title">Add a New Food</div>
+
+            <label>Name</label>
+            <input onChange = {e=>this.handleNameChange(e)} value={this.state.foodNameInput}type="text" />
+
+            <label>Calories</label>
+            <input  onChange = {e=>this.handleCaloriesChange(e)} value={this.state.caloriesInput} type="number" />
+
+            <label>Image</label>
+            <input  onChange = {e=>this.handleImageChange(e)} value={this.state.imageInput} type="text" />
+
+            <button>Submit</button>
+          </form>
+          </div>
+
+        </div>
+
+        <div className="list">{this.showFoods()}</div>
       </div>
     );
   }
